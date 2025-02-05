66149ede7917f8fbcea2ec4dc18dc8233ebe3ed0
Vadimka180880 <vadim.te180880@gmail.com>
2025-02-04 21:55:58 -0500

    1

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,17 +1,12 @@
 // src/components/Product.jsx
 
-export default function Product() {
-  const price = 999;
-
-  return (
-    <>
-      <h2>Tacos</h2>
-      <img
-        src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-        alt="Tacos With Lime"
-        width="640"
-      />
-      <p>Price: {price} credits</p>
-    </>
-  );
-}
+export default function Product({ name, imgUrl, price }) {
+        return (
+          <div>
+            <h2>{name}</h2>
+              <img src={imgUrl} alt={name} width="480" />
+              <p>Price: {price} credits</p>
+          </div>
+        );
+      };
+      
