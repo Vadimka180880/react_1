8a25b7887c2dc2283b7035290371fcb36455fc37
Vadimka180880 <vadim.te180880@gmail.com>
2025-02-04 21:44:01 -0500

    1

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,13 +1,17 @@
 // src/components/Product.jsx
 
 export default function Product() {
-	const price = 999;
+  const price = 999;
 
   return (
     <>
       <h2>Tacos</h2>
-	  <img src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640" alt="Tacos With Lime" width="640" />
+      <img
+        src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+        alt="Tacos With Lime"
+        width="640"
+      />
       <p>Price: {price} credits</p>
     </>
   );
-};
+}
